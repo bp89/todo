333c94b4c83cc7873df727d54f6298680f9ab8e1
Vinay Prajapati <vinay.prajapati@intelligrape.com>
2017-09-03 11:13:02 +0530

    added sample rest calls

--- todo-rest/src/main/java/com/softiv/todo/controller/TaskController.java
+++ todo-rest/src/main/java/com/softiv/todo/controller/TaskController.java
@@ -1,46 +1,70 @@
 package com.softiv.todo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.softiv.todo.domain.Task;
 import com.softiv.todo.repository.TaskRepository;
+import lombok.SneakyThrows;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
+@Log
 public class TaskController {
 
     @Autowired
     TaskRepository taskRepository;
 
-    @RequestMapping("/tasks")
-    @ResponseBody
+    @RequestMapping("/list")
+    @SneakyThrows
     public String home() {
-        return "tasks";
+        List<Task> tasks = taskRepository.findAll();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        String jsonInString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(tasks);
+        return jsonInString;
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String create(ModelMap modelMap, @Valid Task task, BindingResult bindingResult) {
+    @RequestMapping(value = "/create", consumes="application/json", method = RequestMethod.POST)
+    @SneakyThrows
+    public String create(@Valid @RequestBody Task task, BindingResult bindingResult) {
+        System.out.println("task"+task);
         if (bindingResult.hasErrors()) {
+            log.info("Errors"+bindingResult.getAllErrors());
             return "Error";
         } else {
             task = taskRepository.save(task);
-            return task.toString();
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+            String jsonInString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(task);
+            return jsonInString;
         }
     }
 
     @RequestMapping("/get")
-    @ResponseBody
     public String get(@RequestParam Long id) {
         String output = "";
         if (id != null) {
             output = taskRepository.getOne(id).toString();
-        } else {
-
         }
         return output;
     }
+
+
+    @RequestMapping("/query")
+    public String query(@RequestParam Long id) {
+        String output = "";
+        if (id != null) {
+            output = taskRepository.getOne(id).toString();
+        }
+        return output;
+    }
+
+
 }
