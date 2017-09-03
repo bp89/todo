781db19b5f7458db454d56da393fb952d478bac7
Vinay Prajapati <vinay.prajapati@intelligrape.com>
2017-09-03 11:14:39 +0530

    added active profile log

--- todo-rest/src/main/java/com/softiv/todo/TodoApplication.java
+++ todo-rest/src/main/java/com/softiv/todo/TodoApplication.java
@@ -1,15 +1,30 @@
 package com.softiv.todo;
 
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class TodoApplication {
+@Log
+public class TodoApplication implements ApplicationListener<ContextRefreshedEvent> {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+    @Value("${spring.profiles.active}")
+    protected String springProfilesActive;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        log.info("=======================================");
+        log.info("App running with active profiles: " + springProfilesActive);
+        log.info("=======================================");
+    }
 }
